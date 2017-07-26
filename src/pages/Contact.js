ac79eb69157aeb6d9633e804117b501c1c8558fd
Ouss4m4 <bz.oussama@gmail.com>
2017-07-26 12:50:18 +0100

    time to deploy to ghpages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import '../styles/contact.css'
 
+const socialMedia = [
+    {
+        name: 'facebook',
+        link: 'https://www.facebook.com/bz.oussama'
+
+    },
+    {
+        name: 'github',
+        link: 'https://github.com/ouss4m4'
+    },
+    {
+        name: 'twitter',
+        link: 'https://twitter.com/0uss4m4'
+    },
+    {
+        name: 'google-plus',
+        link: 'https://plus.google.com/+OussamaAmassuo'
+    }
+]
 const Contact = () => {
     return (
         <section id='Contact' >
-            <br />
-            <div className='container'>
-                <h1 className="primary"> wtf is this margin up</h1>
+            <div className="socialWrap">
+                <h1 id='contactLead' >I am available for hire. Let's talk </h1>
+                <h3 id='email'><i className='fa fa-envelope'></i> Bz.oussama@gmail.com</h3>
+                <div className='socialItems'>
+                    {socialMedia.map((s,i) => <a key={i} href={s.link}><i className={`fa fa-${s.name}`} ></i> </a> )}
+                </div>
             </div>
-
         </section>
     );
 };
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -9,40 +9,40 @@ const work = [
         project: 'DR PERFORMANCE',
         thumbnail: work1,
         link: 'dr-performance.herokuapp.com',
-        desc: 'Dr Performance, a start up that offers awesome cars tunning, but they could not reach majority of clients, and a facebook page was not enough to show the services they have, until they decided to go for a website ! and it is now skyrocketing !'
+        desc: 'Dr Performance, a single page portfolio to boost sales'
     },
     {
         project: 'SnapShot',
         thumbnail: work2,
         link: 'mern-app.herokuapp.com',
-        desc: 'SnapShot ! you woke up this morning, took a very nice picture, now you want to share it, just not on instagram , snapShot is your GO! show it to other amateurs or pros , get a review or maybe a smiley is all you need'
+        desc: 'SnapShot ! a single page app to show your pictures, with authentification'
     },
     {
         project: 'PlaceHolder',
         thumbnail: work3,
         link: '#',
-        desc: 'The guffawed far less wow then this or bucolically chose save groundhog thirsty excellent peevishly far less compulsively some inventoried the darn erroneous much upon thus and smart in that well much to goodness dragonfly '
+        desc: 'The guffawed far less wow then this or bucolically chose save groundhog '
     },
     {
         project: 'PlaceHolder',
         thumbnail: work3,
         link: '#',
-        desc: 'The guffawed far less wow then this or bucolically chose save groundhog thirsty excellent peevishly far less compulsively some inventoried the darn erroneous much upon thus and smart in that well much to goodness dragonfly '
-    
+        desc: 'The guffawed far less wow then this or bucolically chose save groundhog '
+
     },
     {
         project: 'PlaceHolder',
         thumbnail: work3,
         link: '#',
-        desc: 'The guffawed far less wow then this or bucolically chose save groundhog thirsty excellent peevishly far less compulsively some inventoried the darn erroneous much upon thus and smart in that well much to goodness dragonfly '
-    
+        desc: 'The guffawed far less wow then this or bucolically chose save groundhog '
+
     },
     {
         project: 'PlaceHolder',
         thumbnail: work3,
         link: '#',
-        desc: 'The guffawed far less wow then this or bucolically chose save groundhog thirsty excellent peevishly far less compulsively some inventoried the darn erroneous much upon thus and smart in that well much to goodness dragonfly '
-    
+        desc: 'The guffawed far less wow then this or bucolically chose save groundhog '
+
     }
 ]
 const Work = () => {
@@ -53,18 +53,18 @@ const Work = () => {
                     <h1 className="info">Here is some of my work !</h1>
                 </div>
                 <div className="workGrid">
-                   {work.map(p =>
-                    <div className="workSingle">
-                        <a href={p.link} >
-                            <div className="workImg">
-                                <img src={p.thumbnail} alt=""/>
-                            </div>
+                    {work.map((p, i) =>
+                        <div key={i} className="workSingle">
+                            <a href={`http://${p.link}`} target='_blank' rel='noopener noreferrer' >
+                                <div className="workImg">
+                                    <img src={p.thumbnail} alt="" />
+                                </div>
+                            </a>
                             <div className="desc">
                                 <p>{p.desc}</p>
                             </div>
-                        </a>
-                    </div>)}
-                </div>    
+                        </div>)}
+                </div>
             </div>
         </section>
     );
